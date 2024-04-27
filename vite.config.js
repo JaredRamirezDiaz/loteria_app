b401e4fc0415c5835398dc286b8b54afc6868925
JaredRamirezDiaz <Efrain_jared@hotmail.com>
2024-04-27 10:16:09 -0600

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://JaredRamirezDiaz.github.io/loteria_app/'
+  base: '/loteria_app/'
 })
